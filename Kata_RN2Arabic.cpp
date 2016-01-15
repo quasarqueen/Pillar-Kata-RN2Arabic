b85043e613870f498fdaf135b7040e50aacaae12
quasarqueen <bbladorn@yahoo.com>
2016-01-15 11:13:47 -0500

    Update Kata_RN2Arabic.cpp
    Working Void function to go from Arabic to Roman Numeral. Probably could work with a loop in there, but this is what i got to work. Will process numbers from 1-1000. I realize that is outside the scope of the kata, i don't see a video game having over 100 volumes. but after i got to 100, it seemed kind of silly not to include 1000.

--- Kata_RN2Arabic.cpp
+++ Kata_RN2Arabic.cpp
@@ -59,6 +59,80 @@ void numConvert::Arab2RN()
     std::string roman;
     cin>>number;
     string numStr = to_string(number);
-
+    const int length = numStr.length();
+    
+    int ones=length-1;
+    int tens=length-2;
+    int huns=length-3;
+    
+    if (length==1)
+    {
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+        
+    }
+    if (length==2)
+    {
+        if (numStr[tens] == '1')  {roman.append("X");}
+        if (numStr[tens] == '2')  {roman.append("XX");}
+        if (numStr[tens] == '3')  {roman.append("XXX");}
+        if (numStr[tens] == '4')  {roman.append("XL");}
+        if (numStr[tens] == '5')  {roman.append("L");}
+        if (numStr[tens] == '6')  {roman.append("LX");}
+        if (numStr[tens] == '7')  {roman.append("LXX");}
+        if (numStr[tens] == '8')  {roman.append("LXXX");}
+        if (numStr[tens] == '9')  {roman.append("XC");}
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+    }
+    if (length==3)
+    {
+        if (numStr[huns] == '1')  {roman.append("C");}
+        if (numStr[huns] == '2')  {roman.append("CC");}
+        if (numStr[huns] == '3')  {roman.append("CCC");}
+        if (numStr[huns] == '4')  {roman.append("CD");}
+        if (numStr[huns] == '5')  {roman.append("D");}
+        if (numStr[huns] == '6')  {roman.append("DC");}
+        if (numStr[huns] == '7')  {roman.append("DCC");}
+        if (numStr[huns] == '8')  {roman.append("DCCC");}
+        if (numStr[huns] == '9')  {roman.append("CM");}
+        if (numStr[tens] == '1')  {roman.append("X");}
+        if (numStr[tens] == '2')  {roman.append("XX");}
+        if (numStr[tens] == '3')  {roman.append("XXX");}
+        if (numStr[tens] == '4')  {roman.append("XL");}
+        if (numStr[tens] == '5')  {roman.append("L");}
+        if (numStr[tens] == '6')  {roman.append("LX");}
+        if (numStr[tens] == '7')  {roman.append("LXX");}
+        if (numStr[tens] == '8')  {roman.append("LXXX");}
+        if (numStr[tens] == '9')  {roman.append("XC");}
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+    }
+    if (length==4)
+    {
+        roman.append("M");
+    }
+    
     cout<< "Roman Numeral is: " <<roman <<endl;
 }
